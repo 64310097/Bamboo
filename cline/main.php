37ec2b7a6fd96d87bdb149b3390a1ce19dedce1d
Weerapat <64310097@go.buu.ac.th>
2023-10-10 10:57:47 +0700

    frist commit

---
+++ cline/main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bamboo site</title>
+</head>
+<body class="background">
+    <header class="header">
+        <div class="menu" onclick="openMenu()"> 
+            <span class="bar"> </span>
+                <span class="bar"> </span>
+                <span class="bar"> </span>
+        </div>
+        <a class="logo" href="main.html">bamboo</a>
+    <navbar class="navbar">
+        <a  class="login" href="#">login</a>
+        <a  class="signup" href="#">sign up</a>
+    </navbar>
+    </header>
+    
+
+
+    <content class="cons">
+        <a href="Goods/product.php"> product</a>
+
+    </content>
+
+</body>
+</html>
